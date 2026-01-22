6deb480d88d02ab8ba259ee11f9bff82d0c5703c
Israel Crisanto <israel@sophon.xyz>
2026-01-22 20:09:35 +0000

    chore: mapping server config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 apt install -y ripgrep
 # Beads
 curl -fsSL https://raw.githubusercontent.com/steveyegge/beads/main/scripts/install.sh | bash
+
+# Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+nvm install 24
+# Verify the Node.js version:
+node -v # Should print "v24.13.0".
+# Verify npm version:
+npm -v # Should print "11.6.2".
